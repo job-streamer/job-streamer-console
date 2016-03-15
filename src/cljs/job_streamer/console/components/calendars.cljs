387017922d7155ade88f19a84d95aea779b8d7d8
seki yuki <seki.yuki@tis.co.jp>
2016-03-15 13:38:18 +0900

    fix UI:Delete icon change as delete button. fix UI:It can come back to calendar from calendar->new/edit/detail.

--- src/cljs/job_streamer/console/components/calendars.cljs
+++ src/cljs/job_streamer/console/components/calendars.cljs
@@ -5,8 +5,10 @@
             [sablono.core :as html :refer-macros [html]]
             [cljs.core.async :refer [put! <! chan pub sub unsub-all]]
             [clojure.browser.net :as net]
+            [clojure.string :as string]
             [goog.events :as events]
             [goog.ui.Component]
+            [goog.string :as gstring]
             [bouncer.core :as b]
             [bouncer.validators :as v]
             [job-streamer.console.api :as api]
@@ -33,19 +35,46 @@
                            (put! calendars-channel[:delete-calendar calendar])
                            (set! (.-href js/location) "#/calendars"))}))
 
+(def breadcrumb-elements
+  {:calendars {:name "calendars" :href "#/calendars"}
+   :calendars.new {:name "New" :href "#/calendars/new"}
+   :calendars.detail {:name "calendar: %s" :href "#/calendar/%s"}
+   :calendars.edit {:name "Edit: %s" :href "#/calendar/%s/edit"}})
+
+(defcomponent breadcrumb-view [mode owner]
+  (render-state [_ {:keys [calendar-name]}]
+                (html
+                  [:div.ui.breadcrumb
+                   (drop-last
+                     (interleave
+                       (loop [i 1, items []]
+                         (if (<= i (count mode))
+                           (recur (inc i)
+                                  (conj items (if-let [item (get breadcrumb-elements
+                                                                 (->> mode
+                                                                      (take i)
+                                                                      (map name)
+                                                                      (string/join ".")
+                                                                      keyword))]
+                                                [:a.section {:href (gstring/format (:href item) calendar-name)}
+                                                 (gstring/format (:name item) calendar-name)])))
+                           (let [res (keep identity items)]
+                             (conj (vec (drop-last res))
+                                   (into [:div.section.active] (rest (last res)))))))
+                       (repeat [:i.right.chevron.icon.divider])))])))
+
 (defcomponent calendar-detail-view [cal-name owner]
-  (render-state [_ {:keys [calendar]}]
+  (render-state [_ {:keys [calendar mode]}]
                 (html
                   [:div
+                   (om/build breadcrumb-view mode {:init-state {:calendar-name cal-name}})
                    [:div.ui.grid
                     [:div.row
                      [:div.column
                       [:h3.ui.header (:calendar/name calendar)]]]
-
                     [:div.row
                      [:div.column
                       [:div#holiday-selector]]]
-
                     [:div.row
                      [:div.column
                       [:button.ui.basic.button
@@ -85,9 +114,10 @@
                :kalendae nil
                :save-status false
                :save-error nil})
-  (render-state [_ {:keys [kalendae calendar error-map save-error save-status]}]
+  (render-state [_ {:keys [kalendae calendar error-map save-error save-status mode]}]
                 (html
                   [:form.ui.form
+                   (om/build breadcrumb-view mode {:init-state {:calendar-name cal-name}})
                    [:h4.ui.dividing.header (if (:new? calendar) "New calendar" "Edit calendar")]
                    [:div.ui.grid
                     [:div.row
@@ -132,6 +162,10 @@
                     [:div.row
                      [:div.right.aligned.column
                       [:div.field
+                       [:button.ui.black.deny.button
+                        {:on-click (fn [_]
+                                     (set! (.-href js/location) "#/calendars"))}
+                        "Cancel"]
                        [:button.ui.positive.button.submit
                         {:type "button"
                          :on-click (fn [_]
@@ -189,13 +223,14 @@
                                  (when (> (count (:calendar/holidays cal)) 3)
                                    ",...")))]
                      [:td
-                      [:i.red.delete.icon
-                       {:on-click (fn [e]
+                      [:button.ui.red.button
+                       {:type "button"
+                        :on-click (fn [e]
                                     (.preventDefault e)
                                     (put! calendars-channel [:open-dangerously-dialog
                                                              {:ok-handler (fn []
                                                                             (delete-calendar cal calendars-channel))
-                                                              :answer (:calendar/name cal)}]))}]"Delete"]])]])]])))
+                                                              :answer (:calendar/name cal)}]))}"Delete"]]])]])]])))
 
 (defcomponent calendar-dangerously-action-dialog [app owner {:keys [ok-handler cancel-handler answer]}]
   (init-state [_]
@@ -266,15 +301,17 @@
                        (case mode
                          :new (om/build calendar-edit-view (:calendars app) {:state {:mode (:mode app)}
                                                                              :opts {:calendars-channel calendars-channel}})
-                         :detail (om/build calendar-detail-view (:cal-name app) {:opts {:calendars-channel calendars-channel}})
+                         :detail (om/build calendar-detail-view (:cal-name app) {:state {:mode (:mode app)}
+                                                                                 :opts {:calendars-channel calendars-channel}})
                          :edit (om/build calendar-edit-view (:calendars app)
-                                         {:opts {:mode mode
-                                                 :cal-name (:cal-name app)
-                                                 :calendars-channel calendars-channel}})
+                                         {:opts {:cal-name (:cal-name app)
+                                                 :calendars-channel calendars-channel}
+                                          :state {:mode (:mode app)}})
                          ;; default
                          (cond
                            (nil? (:calendars app)) [:img {:src "/img/loader.gif"}]
-                           :default (om/build calendar-list-view (:calendars app) {:opts {:calendars-channel calendars-channel}})))
+                           :default (om/build calendar-list-view (:calendars app) { :state {:mode (:mode app)}
+                                                                                    :opts {:calendars-channel calendars-channel}})))
                        (when dangerously-action-data
                          (om/build calendar-dangerously-action-dialog nil
                                    {:opts (assoc dangerously-action-data
