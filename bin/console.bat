b77444017add5b0d2d152cd82b54492bd9cddc96
kawasima <kawasima1016@gmail.com>
2016-07-14 18:15:30 +0900

    Changes a main file

--- bin/console.bat
+++ bin/console.bat
@@ -4,7 +4,6 @@ pushd %0\..\..
 
 set /p VERSION=<VERSION
 
-java -cp dist\job-streamer-console-%VERSION%.jar;"lib\*" clojure.main -m job-streamer.console.core
+java -cp dist\job-streamer-console-%VERSION%.jar;"lib\*" clojure.main -m job-streamer.console.main
 
 pause
-
