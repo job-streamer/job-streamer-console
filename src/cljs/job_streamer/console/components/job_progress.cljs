076f27773c472211670a979546940cb795dba15a
seki <schokolade0317@gmail.com>
2017-03-30 20:26:22 +0900

    Apply IE

--- src/cljs/job_streamer/console/components/job_progress.cljs
+++ src/cljs/job_streamer/console/components/job_progress.cljs
@@ -61,8 +61,8 @@
 (defcomponent job-progress-view [{:keys [job width height]} owner]
   (will-mount [_]
              (let [latest-execution-id (get-in job [:job/latest-execution :db/id])]
-               (om/set-state! owner :latest-execution-id latest-execution-id)
-               (search-execution owner (:job/name job) latest-execution-id)))
+               (search-execution owner (:job/name job) latest-execution-id)
+               (om/set-state! owner :latest-execution-id latest-execution-id)))
   (did-mount [_]
     (let [ch (chan)]
       (om/set-state! owner :refresh-timer ch)
@@ -77,14 +77,27 @@
       (put! ch :start)))
   (render-state [_ {:keys [step-executions]}]
                 (doall (map coloring-svg step-executions))
-                (html [:svg (cond->
-                              {:dangerouslySetInnerHTML {:__html (:job/svg-notation job)}
-                               :x "0px"
-                               :y "0px"
-                               :viewBox (str "0 0 " (-> job :job/svg-notation calcute-width) " " (-> job :job/svg-notation calcute-height))
-                               :preserveAspectRadio "xMinYMin meet"}
-                              width (assoc :width (str width "px"))
-                              height (assoc :height (str height "px")))])))
+                ;; it does not work on IE by  React Issue #6950
+;;                 (html [:svg (cond->
+;;                               {:dangerouslySetInnerHTML {:__html (:job/svg-notation job)}
+;;                                :x "0px"
+;;                                :y "0px"
+;;                                :viewBox (str "0 0 " (-> job :job/svg-notation calcute-width) " " (-> job :job/svg-notation calcute-height))
+;;                                :preserveAspectRadio "xMinYMin meet"}
+;;                               width (assoc :width (str width "px"))
+;;                               height (assoc :height (str height "px")))])
+                (html [:div {:dangerouslySetInnerHTML
+                             {:__html
+                              (str "<svg "
+                                   "x=\"0px\" "
+                                   "y=\"0px\" "
+                                   "viewbox=\"" "0 0 " (-> job :job/svg-notation calcute-width) " " (-> job :job/svg-notation calcute-height) "\" "
+                                   "preserveAspectRadio=\"xMinYMin meet\" "
+                                   (when width (str "width=\"" width "px\""))
+                                   (when height (str "height=\"" height "px\""))
+                                   ">"
+                                   (:job/svg-notation job)
+                                   "</svg>")}}])))
 
 (defcomponent coloring-meaning-view[_]
   (render [_]
@@ -147,8 +160,10 @@
                                      (let [w (js/window.open (str "/" app-name "/job/" job-name "/edit") name "width=1200,height=800")]
                                        (.addEventListener w "unload" (fn [] (js/setTimeout (fn [] (put! refresh-job-ch true))) 10))))}
                         "Edit"]]]]
-                    [:div
                      (when job
                        (om/build job-progress-view
-                                 {:job job}
-                                 {:react-key "big-job-progress-view"}))]]])))
+                                 ;;IE cant Adjustment automaticly height
+                                 (cond-> {:job job}
+                                         (not= (.. js/window -navigator -userAgent toLowerCase (indexOf "trident")) -1)
+                                         (assoc :height 490))
+                                 {:react-key "big-job-progress-view"}))]])))
